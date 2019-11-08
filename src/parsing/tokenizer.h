e03f7e09f4773a346226dad07c3642159426449a
Philipp Hochmann <hochmann@embedded.rwth-aachen.de>
2019-11-08 10:00:55 +0100

    Removed leftover util files

--- src/parsing/tokenizer.h
+++
@@ -1,7 +0,0 @@
-#include <stdbool.h>
-#include "context.h"
-
-bool is_space(char c);
-bool is_digit(char c);
-bool is_letter(char c);
-bool tokenize(ParsingContext *ctx, char *input, size_t max_tokens, size_t *out_num_tokens, char **out_tokens);
--- src/util/string_util.h
+++
@@ -1,9 +0,0 @@
-#include <stdbool.h>
-
-#include "../parsing/node.h"
-#include "../parsing/parser.h"
-
-size_t ansi_strlen(char *str);
-bool begins_with(char *prefix, char *str);
-void trim(char *str);
-char *perr_to_string(ParserError perr);
