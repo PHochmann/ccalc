433a83c098af45e9d9fbcf8052e080f47679978f
Philipp Hochmann <phil.hochmann@gmail.com>
2021-02-08 19:51:20 +0100

    Additional test cases

--- tests/test_parser.c
+++ tests/test_parser.c
@@ -20,7 +20,7 @@ struct ErrorTest {
     ParserError result;
 };
 
-static const size_t NUM_VALUE_CASES = 41;
+static const size_t NUM_VALUE_CASES = 42;
 static struct ValueTest valueTests[] = {
     // 1. Basic prefix, infix, postfix
     { "2+3",         5 },
@@ -58,25 +58,27 @@ static struct ValueTest valueTests[] = {
     { "sum()",     0 },
     { "sum() + 2", 2 },
     { "3+sum()",   3 },
-    { "sum()2" ,   0 },
-    { "sum()(2)",  0 },
+    { "prod()2" ,  2 },
+    { "prod()(2)", 2 },
     // 5.2. Unary functions
     { "sin(2)",      0.909297426 },
     { "sin(2)*3",    2.727892280 },
     { "sin(-2)%*3", -0.027278922 },
     // 5.3. Binary functions and dynamic arity
-    { "log(2 64, 1+1)", 7 },
-    { "sum(1,2,3)",     6 },
-    { "prod(2,3,4)-4!", 0 },
+    { "log(2 64, 1+1)",              7 },
+    { "sum(1,2,3)",                  6 },
+    { "prod(2,3,4)-4!",              0 },
+    { "sum(sum(1,2),sum(3,4),5)+6", 21 },
     // 6. Going wild
     { "5 .5sin(2)+5pi5", 80.81305990681 },
     { "--(1+sum(ld(--8), --1%+--1%, 2 .2))%+1", 1.0442 },
     { "-sqrt(abs(--2!!*--sum(-1+.2-.2+2, 2^2^3-255, -sum(.1, .9), 1+2)*--2!!))", -4 },
 };
 
-static const size_t NUM_ERROR_CASES = 16;
+static const size_t NUM_ERROR_CASES = 18;
 static struct ErrorTest errorTests[] = {
     { "",          PERR_UNEXPECTED_END_OF_EXPR },
+    { "     ",     PERR_UNEXPECTED_END_OF_EXPR },
     { "()",        PERR_UNEXPECTED_CLOSING_PARENTHESIS },
     { "x+",        PERR_UNEXPECTED_END_OF_EXPR },
     { "root(x,)",  PERR_UNEXPECTED_CLOSING_PARENTHESIS },
@@ -92,6 +94,7 @@ static struct ErrorTest errorTests[] = {
     { "()+2",      PERR_UNEXPECTED_CLOSING_PARENTHESIS },
     { "(+2",       PERR_EXCESS_OPENING_PARENTHESIS },
     { "sin(())",   PERR_UNEXPECTED_CLOSING_PARENTHESIS },
+    { "sin(2,())", PERR_UNEXPECTED_CLOSING_PARENTHESIS }
 };
 
 static const double EPSILON = 0.00000001;
